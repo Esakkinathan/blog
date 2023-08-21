742ecaf7aa3aee625649b11d89e2b5f229c6e0aa
Esakkinathan <105111413+Esakkinathan@users.noreply.github.com>
2023-08-21 19:10:26 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,3 @@
-python3 -m venv virt
-source "virt/bin/activate"
-pip install --upgrade pip
-pip install -r requirements.txt
-python manage.py migrate
-python manage.py collectstatic
+
+source virt/bin/activate
+pip list
